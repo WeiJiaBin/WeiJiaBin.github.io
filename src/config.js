9ebed17a386e501055f340195607d6a7bb79b34a
weI <wjb@jzdinfo.com>
2024-04-04 18:23:22 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,155 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: 'Hello', //英文标题
+    title: '一个正在追梦的人', //中文标题
+  },
+  // page2部分
+  page2: {
+    authorImg: '', // 作者头像page1.jpg
+    xinhui: 'asasdsdsadsad', // 幸会
+    qiuzhi: '技术经理', // 求职意向
+    guanyuwo: 'asdasdasd', // 关于我
+  },
+  // page3部分
+  page3: [
+    {
+      icon: 'icon-tubiao-',
+      title: 'UI设计',
+      msg: ['PS切图、界面排版'],
+    },
+    {
+      icon: 'icon-diannao', // 图标
+      title: '网页制作', // 标题
+      msg: ['响应式页面', 'css、scss动画效果'], //介绍
+    },
+    {
+      icon: 'icon-qianbi1',
+      title: '前端功能',
+      msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染'],
+    },
+    {
+      icon: 'icon-shouji',
+      title: '框架',
+      msg: ['vue框架', 'UI框架、express框架、mysql'],
+    },
+  ],
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [
+      {
+        date: '2020/9——至今', // 时间
+        desc: {
+          // 经历
+          title: '',
+          list: ['', '', ''],
+        },
+      },
+      {
+        date: '2019/9——2020/9',
+        desc: {
+          title: '',
+          list: ['', '', '', '', '', ''],
+        },
+      },
+      {
+        date: '2018/9——2019/9',
+        desc: {
+          title: '',
+          list: ['', '', '', '', ''],
+        },
+      },
+    ],
+    // 我的拓展技能掌握
+    singlelist: [
+      {
+        title: 'bootsrap',
+        text: '了解',
+      },
+      {
+        title: 'Element-ui',
+        text: '掌握',
+      },
+      {
+        title: 'mint-ui',
+        text: '掌握',
+      },
+      {
+        title: 'scss',
+        text: '掌握',
+      },
+    ],
+    // 我的基本技能掌握
+    proresslist: [
+      {
+        title: 'HTML、CSS、SCSS',
+        value: '90%',
+      },
+      {
+        title: 'JS、JQ',
+        value: '90%',
+      },
+      {
+        title: 'Vue',
+        value: '90%',
+      },
+      {
+        title: 'NodeJs',
+        value: '80%',
+      },
+      {
+        title: 'sql',
+        value: '80%',
+      },
+    ],
+  },
+  // page5部分
+  page5: [
+    {
+      title: '项目一',
+      content: '项目一',
+      image: 'box1.png',
+      href: '',
+    },
+    {
+      title: '项目二',
+      content: '项目二',
+      image: 'box3.jpg',
+      href: '',
+    },
+    {
+      title: '项目三',
+      content: '项目三',
+      image: 'box5.jpg',
+      href: '',
+    },
+    {
+      title: '项目四',
+      content: '项目四',
+      image: 'box2.png',
+      href: '',
+    },
+    {
+      title: '项目五',
+      content: '项目五',
+      image: 'box4.png',
+      href: '',
+    },
+    {
+      title: '项目六',
+      content: '项目六',
+      image: 'box6.jpeg',
+      href: '',
+    },
+  ],
+  // page6部分
+  page6: {
+    github: '',
+    bili: '',
+    email: 'Mailto:793048472@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    zhihu: '',
+    weixin: "<img style='width:120px' src='' alt='加载失败'>",
+    qq: "<img style='width:120px' src='' alt='加载失败'>",
+  },
+};
